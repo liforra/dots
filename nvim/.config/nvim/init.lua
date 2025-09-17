53d3bb27cba781dce3b8586fce6479ab0f5a5fe0
liforra <git@liforra.de>
2025-09-17 09:04:08 +0200

    I dont know what happened but i fixed it

--- nvim/.config/nvim/init.lua
+++
@@ -1,8 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-vim.api.nvim_set_keymap(
-  "t", -- mode: terminal
-  "<Esc>", -- lhs
-  [[<C-\><C-n>]], -- rhs (enter Normal mode)
-  { noremap = true, silent = true }
-)
